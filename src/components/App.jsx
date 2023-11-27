2aad1d0f30a59de784690ec93b24749052187e88
k0robova <alonakorobova@ukr.net>
2023-11-27 11:56:57 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,17 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
 export function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    return storedContacts
+      ? JSON.parse(storedContacts)
+      : [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ];
+  });
 
   const [filter, setFilter] = useState('');
 
@@ -61,8 +66,10 @@ export function App() {
 
   useEffect(() => {
     const storedContacts = localStorage.getItem('contacts');
-    if (storedContacts) {
-      setContacts(JSON.parse(storedContacts));
+    const parsedContacts = JSON.parse(storedContacts);
+
+    if (parsedContacts) {
+      setContacts(parsedContacts);
     }
   }, []);
 
@@ -80,88 +87,3 @@ export function App() {
     </div>
   );
 }
-
-// !====================================================================================
-
-// export class App extends React.Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   };
-
-//   formSubmitHandler = data => {
-//     const { name, number } = data;
-
-//     if (
-//       this.state.contacts.some(
-//         contact => contact.name.toLowerCase() === name.toLowerCase()
-//       )
-//     ) {
-//       alert(`${name} is already in contactcs!`);
-//       return;
-//     } else {
-//       const newContact = {
-//         id: nanoid(),
-//         name: name,
-//         number: number,
-//       };
-
-//       this.setState(prevState => ({
-//         contacts: [...prevState.contacts, newContact],
-//       }));
-//     }
-//   };
-//   onFilterValueChange = evt => {
-//     this.setState({ filter: evt.currentTarget.value });
-//   };
-
-//   getVisibleAllContacts = () => {
-//     return this.state.contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
-//     );
-//   };
-
-//   deleteContact = contactId => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-//     }));
-//   };
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (this.state.contacts !== prevState.contacts) {
-//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//     }
-//   }
-
-//   componentDidMount() {
-//     const contacts = localStorage.getItem('contacts');
-//     const parsedContacts = JSON.parse(contacts);
-//     if (parsedContacts) {
-//       this.setState({ contacts: parsedContacts });
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1 className={css.main_title}>Phonebook</h1>
-//         <Form onSubmitForm={this.formSubmitHandler} />
-
-//         <h2 className={css.second_title}>Contacts</h2>
-//         <Filter
-//           value={this.state.filter}
-//           onFilterValueChange={this.onFilterValueChange}
-//         />
-//         <ContactList
-//           contacts={this.getVisibleAllContacts()}
-//           onDeleteContact={this.deleteContact}
-//         />
-//       </div>
-//     );
-//   }
-// }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,46 +2,27 @@ import React from 'react';
 import { useState } from 'react';
 import css from './Form.module.css';
 
-// const [name, setName] = useLocalStorage('email', '');
-// const [number, setNumber] = useLocalStorage('number', '');
-
-// const useLocalStorage = (key, valueDefault) => {
-//   const [state, setState] = useState(() => {
-//     return JSON.parse(window.localStorage.getItem(key)) ?? valueDefault;
-//   });
-
-//   useEffect(() => {
-//     window.localStorage.setItem(key, JSON.stringify(state));
-//   }, [key, state]);
-
-//   return [state, setState];
-// };
-
-// usestateSnipet - and Tab for camelCase
-
 export function Form({ onSubmitForm }) {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  const resetForm = () => {
-    setName('');
-    setNumber('');
+  const handleNameChange = evt => {
+    setName(evt.target.value);
   };
 
-  const handleNameSubmit = evt => {
-    const { name, value } = evt.currentTarget;
-    // setName({ [name]: value });
-    if (name === 'name') setName(value);
-    if (name === 'number') setNumber(value);
+  const handleNumberChange = evt => {
+    setNumber(evt.target.value);
   };
 
   const handleFormSubmit = evt => {
     evt.preventDefault();
-    onSubmitForm({
-      name,
-      number,
-    });
+    onSubmitForm({ name, number });
     resetForm();
+  };
+
+  const resetForm = () => {
+    setName('');
+    setNumber('');
   };
 
   return (
@@ -52,7 +33,7 @@ export function Form({ onSubmitForm }) {
           type="text"
           name="name"
           value={name}
-          onChange={handleNameSubmit}
+          onChange={handleNameChange}
         />
       </label>
       <label>
@@ -62,7 +43,7 @@ export function Form({ onSubmitForm }) {
           name="number"
           required
           value={number}
-          onChange={handleNameSubmit}
+          onChange={handleNumberChange}
         />
       </label>
       <button type="submit" className={css.btn_submit}>
@@ -71,58 +52,3 @@ export function Form({ onSubmitForm }) {
     </form>
   );
 }
-
-// !================================================================
-// export class Form extends React.Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   handleNameChange = evt => {
-//     const { name, value } = evt.currentTarget;
-//     this.setState({ [name]: value });
-//   };
-
-//   handleFormSubmit = evt => {
-//     evt.preventDefault();
-//     this.props.onSubmitForm(this.state);
-//     this.resetForm();
-//   };
-
-//   resetForm = () => {
-//     this.setState({
-//       name: '',
-//       number: '',
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <form onSubmit={this.handleFormSubmit} className={css.main_form}>
-//         <label>
-//           Name
-//           <input
-//             type="text"
-//             name="name"
-//             value={this.state.name}
-//             onChange={this.handleNameChange}
-//           />
-//         </label>
-//         <label>
-//           Number
-//           <input
-//             type="tel "
-//             name="number"
-//             required
-//             value={this.state.number}
-//             onChange={this.handleNameChange}
-//           />
-//         </label>
-//         <button type="submit" className={css.btn_submit}>
-//           Add contact
-//         </button>
-//       </form>
-//     );
-//   }
-// }
